2f20b1624f5f307a1f8d965b16cf3f8bb919412d
DESKTOP-91J2SSD\User <sakuramayisei@gmail.com>
2022-10-21 23:47:01 +0800

    bulid human + bulid scene + camera follow + human walk

---
+++ Assets/Scenes/human.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class human : MonoBehaviour
+{
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // 用方向鍵左右移動槍
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rb.velocity = new Vector3( h * 5f,  0,  v * 5f);
+    }
+}
